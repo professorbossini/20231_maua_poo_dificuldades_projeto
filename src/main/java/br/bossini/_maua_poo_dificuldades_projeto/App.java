f8e904422018a39891e7916ac4cd1dc2d68fa456
Rodrigo Bossini <professorbossini@gmail.com>
2023-05-11 14:54:08 -0300

    Botao com imagem de fundo e texto centralizado

---
+++ src/main/java/br/bossini/_maua_poo_dificuldades_projeto/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.bossini._maua_poo_dificuldades_projeto;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
